94540a2f6989e6a7494688594d9dfd7010bce357
Arvid Berndtsson <103070833+arvid-berndtsson@users.noreply.github.com>
2024-11-03 21:51:08 +0100

    feat: add event loop support (#5)
    Co-authored-by: Arvid Berndtsson <103070833+dotarvid@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 #![allow(unused_imports)]
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    
+    println!("Logs from your program will appear here!");
 
-    for stream in listener.incoming() {
+
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    
+    loop {
+        let stream = listener.accept().await;
+    
         match stream {
-            Ok(mut _stream) => {
+            Ok((mut _stream, _)) => {
                 println!("accepted new connection");
 
-                // A buffer to store the command, we do not need to relocate this every time, which is why we declare it outside the loop
-                let mut buffer = [0; 512];
-
-                loop {
-                    // Read the command from the stream into the buffer
-                    let read_count = _stream.read(&mut buffer).unwrap();
-
-                    // If the read count is 0, the client has disconnected, so break out of the loop
-                    if read_count == 0 {
-                        break;
+                tokio::spawn(async move {
+                    let mut buf = [0; 512];
+                    loop {
+                        let read_count = _stream.read(&mut buf).await.unwrap();
+                        if read_count == 0 {
+                            break;
+                        }
+        
+                        _stream.write(b"+PONG\r\n").await.unwrap();
                     }
-
-                    // Respond to a PING command with PONG
-                    _stream.write(b"+PONG\r\n").unwrap();
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
